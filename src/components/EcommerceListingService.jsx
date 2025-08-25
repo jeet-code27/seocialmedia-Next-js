34cb989a727d17dc87d8d8cf2dd77d981f1e8aee
Your Name <jetendrasinghratore1@gmail.com>
2025-08-25 18:16:51 +0530

    fix

--- src/components/EcommerceListingService.jsx
+++ src/components/EcommerceListingService.jsx
@@ -419,9 +419,7 @@ const EcommerceListingService = () => {
                     <span>Start Listing Now</span>
                   </button>
                 </Link>
-                <button className="border-2 border-white text-white px-8 py-4 rounded-lg font-semibold hover:bg-white hover:text-blue-600 transition-colors duration-300">
-                  Schedule Consultation
-                </button>
+               
               </div>
             </div>
           </div>
